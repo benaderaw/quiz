c349039e468762ddbd7ecf9741b3743531ccbd1c
Ben Aderaw <benaderaw@Bens-MacBook-Air.local>
2023-12-05 13:08:09 -0500

    added useReducer for count state

--- src/components/DataCounter.jsx
+++ src/components/DataCounter.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  if (action.type === "dec") return state - 1;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "setCount") return action.payload;
+  if (action.type === "reset") return action.payload;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -9,26 +18,24 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
     setStep(Number(e.target.value));
+    // dispatch({ type: "defineStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    dispatch({ type: "reset", payload: 0 });
   };
 
   return (
